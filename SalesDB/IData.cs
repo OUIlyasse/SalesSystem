f31406067b0c4d001a74ae3cd261ce6a80ef2976
Ilyasse <Ilyasse@DESKTOP-ASPGSOQ>
2022-08-16 01:10:56 +0100

    Create Interface "IData"

---
+++ SalesDB/IData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace SalesDB
+{
+    internal interface IData<T>
+    {
+        bool Add(T entity);
+
+        bool Update(T entity);
+
+        bool Delete(T entity);
+
+        IEnumerable<T> GetMaxID();
+
+        IEnumerable<T> GetAllData();
+
+        IEnumerable<T> GetDataBy(Expression<Func<T, bool>> p);
+    }
+}
